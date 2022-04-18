f31d16433f790a45e26f92d6eb3e7d43f98551cc
OriBashary1 <100477565+OriBashary1@users.noreply.github.com>
2022-04-18 17:26:48 +0300

    Update get_params_login.php

--- get_params_login.php
+++ get_params_login.php
@@ -14,7 +14,6 @@
         $rpn = $_GET["reg_pass_new"];
         if ($un == "Ori Bashary" && $pn == "Ori123" && $tn == "0549480720") {
             echo "<h2> good morning Ori</h2> <br> <h2> your new password is :" . $rpn  . "</h2>";
-            // echo "<h2> your new password is :" + "reg_pass_new" + "</h2>";
         } else {
             echo "Enter Valid Data";
         }
